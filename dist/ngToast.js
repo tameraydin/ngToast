d9e514bdce2b67b1915809fca5fc63008ce29dc3
levithomason <me@levithomason.com>
2015-05-15 21:36:44 -0700

    update build

--- dist/ngToast.js
+++ dist/ngToast.js
@@ -22,6 +22,7 @@
           dismissButtonHtml: '&times;',
           dismissOnClick: true,
           compileContent: false,
+          combineDuplications: false,
           horizontalPosition: 'right', // right, center, left
           verticalPosition: 'top', // top, bottom,
           maxNumber: 0
@@ -34,6 +35,7 @@
           }
 
           this.id = id;
+          this.count = 0;
           this.animation = defaults.animation;
           this.className = defaults.className;
           this.additionalClasses = defaults.additionalClasses;
@@ -80,12 +82,25 @@
               }
             },
             create: function(msg) {
+              msg = (typeof msg === 'object') ? msg : {content: msg};
+
+              if (defaults.combineDuplications) {
+                for (var i = messageStack.length - 1; i >= 0; i--) {
+                  var _msg = messages[i];
+                  var _className = msg.className || 'success';
+
+                  if (_msg.content === msg.content &&
+                      _msg.className === _className) {
+                    messages[i].count++;
+                    return;
+                  }
+                }
+              }
+
               if (defaults.maxNumber > 0 &&
                   messageStack.length >= defaults.maxNumber) {
                 this.dismiss(messageStack[0]);
               }
-
-              msg = (typeof msg === 'object') ? msg : {content: msg};
 
               var newMsg = new Message(msg);
               if (defaults.verticalPosition === 'bottom') {
@@ -128,7 +143,7 @@
             '<div class="ng-toast ng-toast--{{hPos}} ng-toast--{{vPos}} {{animation ? \'ng-toast--animate-\' + animation : \'\'}}">' +
               '<ul class="ng-toast__list">' +
                 '<toast-message ng-repeat="message in messages" ' +
-                  'message="message">' +
+                  'message="message" count="message.count">' +
                   '<span ng-bind-html="message.content"></span>' +
                 '</toast-message>' +
               '</ul>' +
@@ -161,7 +176,8 @@
           transclude: true,
           restrict: 'EA',
           scope: {
-            message: '='
+            message: '=',
+            count: '='
           },
           controller: ['$scope', 'ngToast', function($scope, ngToast) {
             $scope.dismiss = function() {
@@ -177,6 +193,9 @@
                   'ng-bind-html="message.dismissButtonHtml" ' +
                   'ng-click="!message.dismissOnClick && dismiss()">' +
                 '</button>' +
+                '<span ng-if="count" class="ng-toast__message__count">' +
+                  '{{count + 1}}' +
+                '</span>' +
                 '<span ng-if="!message.compileContent" ng-transclude></span>' +
               '</div>' +
             '</li>',
