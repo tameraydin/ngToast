8b5dada0be85333926f84e02edbce165316182ed
yofreke <sparkyjoe94@aim.com>
2016-02-16 07:55:10 -0800

    update dist

--- dist/ngToast.js
+++ dist/ngToast.js
@@ -1,132 +1,135 @@
 /*!
  * ngToast v1.5.6 (http://tameraydin.github.io/ngToast)
- * Copyright 2015 Tamer Aydin (http://tamerayd.in)
- * Licensed under MIT (http://tameraydin.mit-license.org/)
+ * Copyright 2016 Tamer Aydin (http://tamerayd.in)
+ * Licensed under  ()
  */
 (function(window, angular, undefined) {
   'use strict';
 
-  angular.module('ngToast.provider', [])
-    .provider('ngToast', [
-      function() {
-        var messages = [],
-            messageStack = [];
+  var module = angular.module('ngToast.provider', []);
+  module.provider('ngToast', [
+    function() {
+      var messages = [],
+          messageStack = [];
 
-        var defaults = {
-          animation: false,
-          className: 'success',
-          additionalClasses: null,
-          dismissOnTimeout: true,
-          timeout: 4000,
-          dismissButton: false,
-          dismissButtonHtml: '&times;',
-          dismissOnClick: true,
-          compileContent: false,
-          combineDuplications: false,
-          horizontalPosition: 'right', // right, center, left
-          verticalPosition: 'top', // top, bottom,
-          maxNumber: 0
-        };
+      var defaults = {
+        animation: false,
+        className: 'success',
+        additionalClasses: null,
+        dismissOnTimeout: true,
+        timeout: 4000,
+        dismissButton: false,
+        dismissButtonHtml: '&times;',
+        dismissOnClick: true,
+        compileContent: false,
+        combineDuplications: false,
+        horizontalPosition: 'right', // right, center, left
+        verticalPosition: 'top', // top, bottom,
+        maxNumber: 0
+      };
 
-        function Message(msg) {
-          var id = Math.floor(Math.random()*1000);
-          while (messages.indexOf(id) > -1) {
-            id = Math.floor(Math.random()*1000);
-          }
-
-          this.id = id;
-          this.count = 0;
-          this.animation = defaults.animation;
-          this.className = defaults.className;
-          this.additionalClasses = defaults.additionalClasses;
-          this.dismissOnTimeout = defaults.dismissOnTimeout;
-          this.timeout = defaults.timeout;
-          this.dismissButton = defaults.dismissButton;
-          this.dismissButtonHtml = defaults.dismissButtonHtml;
-          this.dismissOnClick = defaults.dismissOnClick;
-          this.compileContent = defaults.compileContent;
-
-          angular.extend(this, msg);
+      var Message = function(msg) {
+        var id = Math.floor(Math.random()*1000);
+        while (messages.indexOf(id) > -1) {
+          id = Math.floor(Math.random()*1000);
         }
 
-        this.configure = function(config) {
-          angular.extend(defaults, config);
+        this.id = id;
+        this.count = 0;
+        this.animation = defaults.animation;
+        this.className = defaults.className;
+        this.additionalClasses = defaults.additionalClasses;
+        this.dismissOnTimeout = defaults.dismissOnTimeout;
+        this.timeout = defaults.timeout;
+        this.dismissButton = defaults.dismissButton;
+        this.dismissButtonHtml = defaults.dismissButtonHtml;
+        this.dismissOnClick = defaults.dismissOnClick;
+        this.compileContent = defaults.compileContent;
+
+        angular.extend(this, msg);
+      };
+
+      this.configure = function(config) {
+        angular.extend(defaults, config);
+      };
+
+      this.$get = ['$rootScope', function($rootScope) {
+        var _createWithClassName = function(className, msg) {
+          msg = (typeof msg === 'object') ? msg : {content: msg};
+          msg.className = className;
+
+          return this.create(msg);
         };
 
-        this.$get = [function() {
-          var _createWithClassName = function(className, msg) {
-            msg = (typeof msg === 'object') ? msg : {content: msg};
-            msg.className = className;
-
-            return this.create(msg);
-          };
-
-          return {
-            settings: defaults,
-            messages: messages,
-            dismiss: function(id) {
-              if (id) {
-                for (var i = messages.length - 1; i >= 0; i--) {
-                  if (messages[i].id === id) {
-                    messages.splice(i, 1);
-                    messageStack.splice(messageStack.indexOf(id), 1);
-                    return;
-                  }
-                }
-
-              } else {
-                while(messages.length > 0) {
-                  messages.pop();
-                }
-                messageStack = [];
-              }
-            },
-            create: function(msg) {
-              msg = (typeof msg === 'object') ? msg : {content: msg};
-
-              if (defaults.combineDuplications) {
-                for (var i = messageStack.length - 1; i >= 0; i--) {
-                  var _msg = messages[i];
-                  var _className = msg.className || 'success';
-
-                  if (_msg.content === msg.content &&
-                      _msg.className === _className) {
-                    messages[i].count++;
-                    return;
-                  }
+        return {
+          settings: defaults,
+          messages: messages,
+          dismiss: function(id) {
+            if (id) {
+              for (var i = messages.length - 1; i >= 0; i--) {
+                if (messages[i].id === id) {
+                  messages.splice(i, 1);
+                  messageStack.splice(messageStack.indexOf(id), 1);
+                  $rootScope.$broadcast('ngToast:update');
+                  return;
                 }
               }
 
-              if (defaults.maxNumber > 0 &&
-                  messageStack.length >= defaults.maxNumber) {
-                this.dismiss(messageStack[0]);
+            } else {
+              while(messages.length > 0) {
+                messages.pop();
               }
-
-              var newMsg = new Message(msg);
-              if (defaults.verticalPosition === 'bottom') {
-                messages.unshift(newMsg);
-              } else {
-                messages.push(newMsg);
-              }
-              messageStack.push(newMsg.id);
-              return newMsg.id;
-            },
-            success: function(msg) {
-              return _createWithClassName.call(this, 'success', msg);
-            },
-            info: function(msg) {
-              return _createWithClassName.call(this, 'info', msg);
-            },
-            warning: function(msg) {
-              return _createWithClassName.call(this, 'warning', msg);
-            },
-            danger: function(msg) {
-              return _createWithClassName.call(this, 'danger', msg);
+              messageStack = [];
+              $rootScope.$broadcast('ngToast:update');
             }
-          };
-        }];
-      }
-    ]);
+          },
+          create: function(msg) {
+            msg = (typeof msg === 'object') ? msg : {content: msg};
+
+            if (defaults.combineDuplications) {
+              for (var i = messageStack.length - 1; i >= 0; i--) {
+                var _msg = messages[i];
+                var _className = msg.className || 'success';
+
+                if (_msg.content === msg.content &&
+                    _msg.className === _className) {
+                  messages[i].count++;
+                  return;
+                }
+              }
+            }
+
+            if (defaults.maxNumber > 0 &&
+                messageStack.length >= defaults.maxNumber) {
+              this.dismiss(messageStack[0]);
+            }
+
+            var newMsg = new Message(msg);
+            if (defaults.verticalPosition === 'bottom') {
+              messages.unshift(newMsg);
+            } else {
+              messages.push(newMsg);
+            }
+            messageStack.push(newMsg.id);
+            $rootScope.$broadcast('ngToast:update');
+            return newMsg.id;
+          },
+          success: function(msg) {
+            return _createWithClassName.call(this, 'success', msg);
+          },
+          info: function(msg) {
+            return _createWithClassName.call(this, 'info', msg);
+          },
+          warning: function(msg) {
+            return _createWithClassName.call(this, 'warning', msg);
+          },
+          danger: function(msg) {
+            return _createWithClassName.call(this, 'danger', msg);
+          }
+        };
+      }];
+    }
+  ]);
 
 })(window, window.angular);
 
@@ -186,13 +189,17 @@
               scope.vPos = ngToast.settings.verticalPosition;
               scope.animation = ngToast.settings.animation;
               scope.messages = ngToast.messages;
+
+              scope.$on('ngToast:update', function() {
+                scope.$applyAsync();
+              });
             };
           }
         };
       }
     ])
-    .directive('toastMessage', ['$timeout', '$compile', 'ngToast',
-      function($timeout, $compile, ngToast) {
+    .directive('toastMessage', ['$timeout', '$compile', '$templateRequest', '$controller', '$rootScope', 'ngToast',
+      function($timeout, $compile, $templateRequest, $controller, $rootScope, ngToast) {
         return {
           replace: true,
           transclude: true,
@@ -211,7 +218,13 @@
             element.attr('data-message-id', scope.message.id);
 
             var dismissTimeout;
+            var templateUrl = scope.message.templateUrl;
             var scopeToBind = scope.message.compileContent;
+
+            // Default scope for templates
+            if (templateUrl && !scopeToBind) {
+              scopeToBind = $rootScope.$new(true);
+            }
 
             scope.cancelTimeout = function() {
               $timeout.cancel(dismissTimeout);
@@ -241,13 +254,36 @@
                 element.children().append(transcludedEl);
               });
 
-              $timeout(function() {
-                $compile(transcludedEl.contents())
-                  (typeof scopeToBind === 'boolean' ?
-                    scope.$parent : scopeToBind, function(compiledClone) {
-                    transcludedEl.replaceWith(compiledClone);
-                  });
-              }, 0);
+              var doLink = function(contents) {
+                var template = angular.element(contents);
+                // element.append(template);
+                var linkFn = $compile(template);
+                var linkedScope = typeof scopeToBind === 'boolean' ?
+                    scope.$parent : scopeToBind;
+                var cloneConnectFn = function(compiledClone) {
+                  transcludedEl.replaceWith(compiledClone);
+                };
+                var controller = scope.message.controller;
+                var linkOpts = {};
+                if (controller) {
+                  linkOpts.transcludeControllers = {};
+                  var locals = angular.extend({
+                    $scope: linkedScope
+                  }, scope.message.locals);
+                  linkOpts.transcludeControllers[controller] = $controller(controller, locals);
+                }
+                linkFn(linkedScope, cloneConnectFn, linkOpts);
+              };
+
+              if (templateUrl) {
+                $templateRequest(templateUrl).then(function(templateHtml) {
+                  return doLink(templateHtml);
+                });
+              } else {
+                $timeout(function() {
+                  doLink(transcludedEl.contents());
+                }, 0);
+              }
             }
 
             scope.startTimeout();
